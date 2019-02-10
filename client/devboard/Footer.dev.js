9e120a0db7071ce03ac7b1b5b175eb845e567530
UIProf <cis.roshan27ag@gmail.com>
2019-02-11 00:57:21 +1100

    work code

---
+++ client/devboard/Footer.dev.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import devboard from "devboard";
+
+const definecard = devboard.ns("3. <Footer />");
+
+import DemoBox from "./DemoBox";
+import Footer from "../components/Footer";
+
+definecard(
+  `
+  This component represents the whole footer
+  `
+);
+
+definecard("Sample",
+  <DemoBox>
+    <Footer total={5} completed={3} />
+  </DemoBox>
+);
+
+definecard("Sample 1",
+  <DemoBox>
+    <Footer total={5} selectFilter={1} completed={3}/>
+  </DemoBox>
+);
+
+
+definecard("Add your own here...");
